dac8ffb11287f3577daea81a022adea040568e3d
unknown <gabriel_picaz@estudante.sc.senai.br>
2020-05-21 13:41:50 -0300

    ppp

---
+++ src/components/Infracoes.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="infracoes">
+
+      <div v-for="(infracao, index) in infracoes" :key="index">
+        <label for="data">Data da infração:</label>
+        <input type="text" id="data" :value="infracao.DADOS_INFRACAO.DATAHORA" /><br>
+
+        <label for="placa">Placa:</label>
+        <input type="text" id="placa" :value="infracao.PLACA" /><br>
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    infracoes: function() {
+        return this.$store.state.infracoesVeiculo
+    }  
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Pesquisa.vue
@@ -0,0 +1,30 @@
+<template>
+  <fieldset>
+    <legend>Dados do veiculos</legend>
+    <label for="placa">Placa:</label>
+    <input type="text" id="placa" placeholder="informe a placa" maxlength="8" v-model="placa" />
+    <button @click="pesquisarVeiculo">OK</button>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      placa: ""
+    };
+  },
+  methods: {
+    pesquisarVeiculo: function() {
+      console.log("passou consulta")        
+      this.$store.state.infracoesVeiculo = this.$store.state.multas.filter(
+        multa => multa.PLACA == this.placa
+      );
+      this.$router.push("/infracoes")
+    }
+  }
+};
+</script>
+
+<style>
+</style>
